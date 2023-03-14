02a9538792f11afda013c5c5f07928cf7dc7dd1f
lasha tsulukidze <121624660+LashaTsulukidze751@users.noreply.github.com>
2023-03-14 18:44:44 +0400

    Add files via upload

--- samkutxedebis fartobi/Untitled1.cpp
+++ samkutxedebis fartobi/Untitled1.cpp
@@ -19,6 +19,15 @@ double fartobi_2(double A, double B, double C) {
      return 0.5*A*B*sin(rad); 
 }
 //mesameze unda gamogvetvala ori kutxita da 1 gverdit da formula ar vici
+double perimetri_1(double A,double B, double C){
+	return A+B+C;
+}
+double perimetri_2(double A,double B, double C){
+	double rad= C * M_PI / 180.0;
+	int mesameGverdi;
+	mesameGverdi=sqrt(A*A+2*A*B*cos(rad)+B*B);
+	return A+B+C;
+}
 int main() {
     tringle c;
     cout<<"gverdi1=";cin>>c.gverdi1;
@@ -27,8 +36,8 @@ int main() {
     cout<<"kutxe1=";cin>>c.kutxe1;    
     cout<<"kutxe2=";cin>>c.kutxe2; 
     cout<<"kutxe3=";cin>>c.kutxe3; 
-    cout<<"sami gverdit migebuli fartobi="<<fartobi_1(c.gverdi1, c.gverdi2, c.gverdi3)<<endl;
-     cout<<"ori gverdita da 1kutxit migebuli fartobi="<<fartobi_2(c.gverdi1, c.gverdi2, c.kutxe1)<<endl;
+    cout<<"sami gverdit migebuli samkutxedis fartobia="<<fartobi_1(c.gverdi1, c.gverdi2, c.gverdi3)<<endl;
+     cout<<"ori gverdita da 1kutxit migebuli samkutxedis fartobia="<<fartobi_2(c.gverdi1, c.gverdi2, c.kutxe1)<<endl;
     //imistvis rom shevamowmot emtxveva tu ara pasuxebi chaweret ricxvebi zusti tanmimdevrobit. 
 	//magalitad gverdi1=3
 	//gverdi2=4
@@ -37,6 +46,7 @@ int main() {
 	//kutxe2=67.4
 	//kutxe3=23.1
 	//es aris zusti kutxeebi im shemtxvevashi tu samkutxedis gverdebia 3,4,5 :)
-
+	cout<<"sami gverdit migebuli samkutxedis perimetria="<<perimetri_1(c.gverdi1, c.gverdi2, c.gverdi3)<<endl;
+	cout<<"sami gverdita da 1kutxit  migebuli samkutxedis perimetria="<<perimetri_2(c.gverdi1, c.gverdi2, c.gverdi3)<<endl;
     return 0;
 }
